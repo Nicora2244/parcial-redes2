8c1d2fc8fe3526da97ac1bdae3fae8f55b26d8ab
Nicolas Londono <nicksiuxs@gmail.com>
2024-11-07 20:23:31 -0500

    Add: some dependencies and setups

--- userService/src/index.js
+++ userService/src/index.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 const express = require("express");
-
+const morgan = require("morgan");
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`Microservicio Usuario ejecutandose en el puerto 3000`);
+app.use(morgan("dev"));
+app.use(express.json());
+
+const PORT_USER = process.env.PORT_USER || 3002;
+
+app.listen(PORT_USER, () => {
+    console.log(`Microservicio Usuario ejecutandose en el puerto ${PORT_USER}`);
 });
