50e9c79bba28c7543399d18540945d1c1a0e4b2c
Nicolas Londono <nicksiuxs@gmail.com>
2024-11-07 21:21:08 -0500

    Add: create user endpoint

--- userService/src/controller/userController.js
+++ userService/src/controller/userController.js
@@ -1,12 +1,36 @@
 const { Router } = require("express");
+const { check, validationResult } = require("express-validator");
 const router = Router();
 const userModel = require("../model/userModel");
+const { createResponse } = require("../../../utils/utils");
+
+const validRoles = ["admin", "user"];
+
+// POST a new user
+router.post("/user",
+    [
+        check("full_name").notEmpty().withMessage("Full name is required").isString().withMessage("Full name must be a string"),
+        check("username").notEmpty().withMessage("Username is required").isString().withMessage("Username must be a string"),
+        check("password").notEmpty().withMessage("Password is required").isString().withMessage("Password must be a string"),
+        check("role").notEmpty().withMessage("Role is required").isString().withMessage("Role must be a string").isIn(validRoles).withMessage(`Role must be one of the following: ${validRoles.join(", ")}`),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(createResponse("error", null, errors.array()));
+        }
+        try {
+            const result = await userModel.createUser(req.body);
+            res.status(201).json(createResponse("success", result, "User created successfully"));
+        } catch (error) {
+            res.status(500).json(createResponse("error", null, error.message));
+        }
+    });
 
 // GET all users
 router.get("/user", async (req, res) => {
     const result = await userModel.getUsers();
-    res.json(result);
+    res.status(200).json(createResponse('success', result));
 });
-
 
 module.exports = router;
--- userService/src/model/userModel.js
+++ userService/src/model/userModel.js
@@ -12,10 +12,8 @@ const connection = mysql.createPool({
     database: DB_NAME_USER,
 });
 
-
 /**
  * Retrieves all users from the database.
- * @function getUsers
  * @returns {Promise<Array>} A promise that resolves to an array of user objects.
  */
 async function getUsers() {
@@ -23,4 +21,27 @@ async function getUsers() {
     return result[0];
 }
 
-module.exports = { getUsers };
+/**
+ * Creates a new user in the database.
+ * @param {Object} newUser - The new user to be created.
+ * @param {string} newUser.full_name - The full name of the user.
+ * @param {string} newUser.username - The username of the user.
+ * @param {string} newUser.password - The password of the user.
+ * @param {string} newUser.role - The role of the user.
+ * @returns {Promise<Object>} The created user with the new ID.
+ * @throws {Error} If there is an error during the database query.
+ */
+async function createUser(newUser) {
+    try {
+        const [result] = await connection.query(
+            "INSERT INTO users (full_name, username, password, role) VALUES (?, ?, ?, ?)",
+            [newUser.full_name, newUser.username, newUser.password, newUser.role]
+        );
+
+        return [{ id: result.insertId, ...newUser }];
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+module.exports = { getUsers, createUser };
